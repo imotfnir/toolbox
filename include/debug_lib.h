3e08359a915f54e15b54bd8949a07dabd474219f
Bao.Pang.ArchLinux <bao.pang@ufispace.com>
2022-12-02 21:21:52 +0800

    feat(pcie): add debug level marco

--- include/debug_lib.h
+++ include/debug_lib.h
@@ -6,10 +6,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Marco Variable
+#define DEBUG_FATAL 1
+#define DEBUG_ERROR 2
+#define DEBUG_WARN 3
+#define DEBUG_INFO 4
+#define DEBUG_DEBUG 5
+#define DEBUG_TRACE 6
+
+// Marco Function
 #define FATAL                                                                                                 \
     do {                                                                                                      \
         fprintf(stderr, "Error at line %d, file %s (%d) [%s]\n", __LINE__, __FILE__, errno, strerror(errno)); \
         exit(1);                                                                                              \
     } while(0)
+
+void debug(__uint8_t debug_level, char *format, ...);
 
 #endif //_DEBUG_LIB_H_
