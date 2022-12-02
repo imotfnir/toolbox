ed654dd110c25ade4238451ca7e97276a6a2fdb3
Bao.Pang.ArchLinux <bao.pang@ufispace.com>
2022-12-02 22:49:48 +0800

    feat(pcie): add mmio_read instance in io_lib

--- library/io_lib.c
+++ library/io_lib.c
@@ -1,7 +1,11 @@
 #include <debug_lib.h>
 #include <io_lib.h>
 
+#include <fcntl.h>
 #include <stdbool.h>
+#include <sys/io.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 
 __uint8_t io_read(__uint16_t address) {
     __uint8_t value;
@@ -29,9 +33,13 @@ __uint8_t io_read(__uint16_t address) {
 
 __uint8_t mmio_read(__uint64_t address) {
     int fd;
+    void *map_base;
 
     if((fd = open("/dev/mem", O_RDONLY | O_SYNC)) < 0) FATAL;
 
+    if((map_base = mmap(0, 1, PROT_READ, MAP_SHARED, fd, address)) == MAP_FAILED) FATAL;
+    TRACE_PRINT("mmap at address %p", map_base);
+
     close(fd);
-    return 0;
+    return *((__uint8_t *)map_base);
 }
--- pcie_tools/pcie_tools.c
+++ pcie_tools/pcie_tools.c
@@ -6,6 +6,8 @@
 int main() {
     int test;
     test = io_read(0x600);
-    printf("%d", test);
+    printf("0x%x\n", test);
+    test = mmio_read(0xa0000);
+    printf("0x%x\n", test);
     return 0;
 }
