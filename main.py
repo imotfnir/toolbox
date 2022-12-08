31ec841208ee4b926cf038be79cbf18d20a9809d
Bao.Pang.ArchLinux <bao.pang@ufispace.com>
2022-12-08 22:22:57 +0800

    feat(autotest): first commit for autotest tools, a demo for auto login to console

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import src.console_connection as cc
+import time
+
+
+PROMPT = "root@ubuntu:~#"
+console = cc.console_connection(
+    ip='192.168.162.2',
+    username='administrator',
+    password='ufispace',
+    port='5101')
+
+console.send_command("\r\n")
+
+console.wait_for_pattern("ubuntu login:")
+console.send_command("root")
+console.wait_for_pattern("Password:")
+console.send_command("ufispace")
+
+# console.wait_for_pattern(PROMPT)
+# console.send_command("dmidecode -s bios-version")
+# console.wait_for_pattern(PROMPT)
+# console.send_command("lscpu")
+# console.wait_for_pattern(PROMPT)
