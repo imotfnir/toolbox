798f34c311ee332e1e0a6e6ffecd10a49d91ebaa
PangCP <chinpokpang@gmail.com>
2022-12-10 00:05:28 +0800

    refactor(library, autotest): use decorator to implement getter,setter for Ipv4

--- library/custom_type.py
+++ library/custom_type.py
@@ -8,17 +8,21 @@ class Ipv4:
         self._port = port
         return
 
-    def get_ip(self) -> str:
+    @property
+    def ip(self) -> str:
         return self._ip
 
-    def set_ip(self, ip: str) -> None:
+    @ip.setter
+    def ip(self, ip: str) -> None:
         self._ip = ipaddress.ip_address(ip).__str__()
         return
 
-    def get_port(self) -> int:
+    @property
+    def port(self) -> int:
         return self._port
 
-    def set_port(self, port: int) -> None:
+    @port.setter
+    def port(self, port: int) -> None:
         if (port < 1):
             logging.warning("port number only can be 5101~5116")
             return
--- test/test_custom_type.py
+++ test/test_custom_type.py
@@ -6,38 +6,35 @@ ipv4 = Ipv4(ip='192.168.162.2', port=5102)
 
 
 def test_ipv4_port() -> None:
-    assert ipv4.get_port() == 5102
-    ipv4.set_port(2000)
-    assert ipv4.get_port() == 5102
-    ipv4.set_port(1)
-    assert ipv4.get_port() == 5101
-    ipv4.set_port(5100)
-    assert ipv4.get_port() == 5101
-    ipv4.set_port(5116)
-    assert ipv4.get_port() == 5116
+    assert ipv4.port == 5102
+    ipv4.port = 2000
+    assert ipv4.port == 5102
+    ipv4.port = 1
+    assert ipv4.port == 5101
+    ipv4.port = 5100
+    assert ipv4.port == 5101
+    ipv4.port = 5116
+    assert ipv4.port == 5116
     return
 
 
 def test_ipv4_ip() -> None:
-    ipv4.set_ip("10.60.120.10")
-    assert ipv4.get_ip() == "10.60.120.10"
-    ipv4.set_ip("255.255.255.255")
-    assert ipv4.get_ip() == "255.255.255.255"
+    ipv4.ip = "10.60.120.10"
+    assert ipv4.ip == "10.60.120.10"
+    ipv4.ip = "255.255.255.255"
+    assert ipv4.ip == "255.255.255.255"
     return
 
 
 def test_ipv4_ip_exception() -> None:
     with pytest.raises(ValueError):
-        ipv4.set_ip("255.255.1.320")
+        ipv4.ip = "255.255.1.320"
     with pytest.raises(ValueError):
-        ipv4.set_ip("3221225985")
+        ipv4.ip = "3221225985"
     with pytest.raises(ValueError):
-        ipv4.set_ip("255.255")
+        ipv4.ip = "255.255"
     return
 
 
 if __name__ == "__main__":
-    ipv4.set_ip("10.60.120.10")
-    print(type("110.60.150.10"))
-    ipv4.set_ip("255.255.255.255")
-    print(ipv4.get_ip())
+    pass
