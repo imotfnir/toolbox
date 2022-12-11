4f2ef08e82812b79c3ea86c336c6102c6608678e
PangCP <chinpokpang@gmail.com>
2022-12-11 23:28:46 +0800

    test(library): post-condition test for Ipv4's child

--- test/test_custom_type.py
+++ test/test_custom_type.py
@@ -7,7 +7,7 @@ console_obj = ConsoleIp(ip="192.168.0.1", port=5101)
 ssh_obj = SshIp(ip="192.168.0.1", port=22)
 
 
-def ipv4(obj: Ipv4) -> None:
+def ipv4_item(obj: Ipv4) -> None:
     obj.port = 5102
     assert obj.port == 5102
     obj.port = 2000
@@ -25,7 +25,7 @@ def ipv4(obj: Ipv4) -> None:
     return
 
 
-def ipv4_exception(obj: Ipv4) -> None:
+def ipv4_exception_item(obj: Ipv4) -> None:
     with pytest.raises(ValueError):
         obj.port = 65536
     with pytest.raises(ValueError):
@@ -39,51 +39,65 @@ def ipv4_exception(obj: Ipv4) -> None:
     return
 
 
+def console_item(obj: Ipv4) -> None:
+    obj.ip = "10.10.10.10"
+    assert obj.ip == "10.10.10.10"
+    obj.port = 5101
+    assert obj.port == 5101
+    obj.port = 5116
+    assert obj.port == 5116
+    return
+
+
+def ssh_item(obj: Ipv4) -> None:
+    obj.ip = "10.58.151.139"
+    assert obj.ip == "10.58.151.139"
+    obj.port = 22
+    assert obj.port == 22
+    return
+# Test
+
+
 def test_ipv4() -> None:
-    ipv4(ipv4_obj)
+    ipv4_item(ipv4_obj)
+    console_item(ipv4_obj)
     return
 
 
 def test_ipv4_exception() -> None:
-    ipv4_exception(ipv4_obj)
+    ipv4_exception_item(ipv4_obj)
     return
 
 
 def test_console() -> None:
-    console_obj.ip = "10.10.10.10"
-    assert console_obj.ip == "10.10.10.10"
-    console_obj.port = 5101
-    assert console_obj.port == 5101
-    console_obj.port = 2
-    assert console_obj.port == 5102
+    console_item(console_obj)
     return
 
 
 def test_console_exception() -> None:
-    ipv4_exception(console_obj)
+    ipv4_exception_item(console_obj)
     with pytest.raises(ValueError):
-        console_obj.ip = "259.10.23.5"
+        console_obj.port = 5117
     with pytest.raises(ValueError):
-        console_obj.port = 8080
+        console_obj.port = 5100
     with pytest.raises(ValueError):
         console_obj.port = 2000
     return
 
 
 def test_ssh() -> None:
-    ssh_obj.ip = "10.58.151.139"
-    assert ssh_obj.ip == "10.58.151.139"
-    with pytest.raises(ValueError):
-        ssh_obj.ip = "112.114.555.5"
+    ssh_item(ssh_obj)
     return
 
 
 def test_ssh_exception() -> None:
-    ipv4_exception(ssh_obj)
-    ssh_obj.port = 22
-    assert ssh_obj.port == 22
+    ipv4_exception_item(ssh_obj)
     with pytest.raises(ValueError):
         ssh_obj.port = 8080
+    with pytest.raises(ValueError):
+        ssh_obj.port = 23
+    with pytest.raises(ValueError):
+        ssh_obj.port = 21
     return
 
 
