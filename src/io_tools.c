1660eac3e73f69deb18bc1bf8254d4054f02fc0a
PangCP <chinpokpang@gmail.com>
2022-12-16 02:00:38 +0800

    feat(library, iotools): use rw_config object to save input arvgment

--- src/io_tools.c
+++ src/io_tools.c
@@ -11,8 +11,12 @@ void show_help();
 void show_version();
 unsigned long djb_hash(char *str);
 bool is_flag_supported(char *flag);
+bool set_digit_args_to_config(rw_config *cfg, int val);
+bool set_width_to_config(rw_config *cfg, char *flag);
+bool set_mode_to_config(rw_config *cfg, char *flag);
 
-const char *flag_list[] = { "-h", "--help", "-v", "--version", "-w", "--width", "-io", "-mmio", "-pci", "-cc", "-mc" };
+const char *flag_list[]
+    = { "-h", "--help", "-v", "--version", "-w", "--width", "-io", "-mmio", "-pci", "-cc", "-mc" };
 
 void show_help() {
     // ToDo
@@ -30,9 +34,7 @@ unsigned long djb_hash(char *str) {
     unsigned long hash = 5381;
     int c;
 
-    while((c = *str++)) {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
+    while((c = *str++)) { hash = ((hash << 5) + hash) + c; /* hash * 33 + c */ }
 
     return hash;
 }
@@ -40,13 +42,56 @@ unsigned long djb_hash(char *str) {
 bool is_flag_supported(char *flag) {
     const int len = sizeof(flag_list) / sizeof(flag_list[0]);
     for(size_t i = 0; i < len; i++) {
-        if(strcasecmp(flag_list[i], flag) == 0)
-            return true;
+        if(strcasecmp(flag_list[i], flag) == 0) return true;
     }
+    debug_print(DEBUG_ERROR, "Argument %s not supported\n", flag);
     return false;
 }
 
-bool set_mode(rw_mode *mode, char *flag) {
+bool set_digit_args_to_config(rw_config *cfg, int val) {
+    static size_t digit_args_count = 0;
+    if(digit_args_count >= 2) {
+        debug_print(DEBUG_ERROR, "Too many data\n");
+        return false;
+    }
+    if(digit_args_count == 0) {
+        cfg->address = val;
+        debug_print(DEBUG_DEBUG, "Address = 0x%x\n", cfg->address);
+    }
+    if(digit_args_count == 1) {
+        cfg->data = val;
+        debug_print(DEBUG_DEBUG, "Address = 0x%x\n", cfg->data);
+    }
+
+    digit_args_count++;
+    return true;
+}
+
+bool set_width_to_config(rw_config *cfg, char *flag) {
+    int val;
+    char *success;
+
+    val = strtoul(flag, &success, 0);
+    if(!*success == '\0') {
+        debug_print(DEBUG_DEBUG, "%s not a digit\n", flag);
+        return false;
+    }
+    switch(val) {
+    case io_width_8:
+    case io_width_16:
+    case io_width_32:
+        cfg->width = val;
+        break;
+    case io_width_64:
+    default:
+        debug_print(DEBUG_DEBUG, "Width not support\n");
+        break;
+    }
+    debug_print(DEBUG_INFO, "Width = %d\n", cfg->width);
+    return true;
+}
+
+bool set_mode_to_config(rw_config *cfg, char *flag) {
     static bool is_mode_setted = false;
 
     if(is_mode_setted) {
@@ -54,28 +99,26 @@ bool set_mode(rw_mode *mode, char *flag) {
         return false;
     }
 
-    if(strcasecmp("-io", flag) == 0)
-        *mode = io;
-    if(strcasecmp("-mmio", flag) == 0)
-        *mode = mmio;
-    if(strcasecmp("-pci", flag) == 0)
-        *mode = pci;
-    if(strcasecmp("-cc", flag) == 0)
-        *mode = cpucpld;
-    if(strcasecmp("-mc", flag) == 0)
-        *mode = mbcpld;
+    if(strcasecmp("-io", flag) == 0) cfg->mode = io;
+    if(strcasecmp("-mmio", flag) == 0) cfg->mode = mmio;
+    if(strcasecmp("-pci", flag) == 0) cfg->mode = pci;
+    if(strcasecmp("-cc", flag) == 0) cfg->mode = cpucpld;
+    if(strcasecmp("-mc", flag) == 0) cfg->mode = mbcpld;
 
     is_mode_setted = true;
+    debug_print(DEBUG_INFO, "Mode = %d, %s\n", cfg->mode, flag);
     return true;
 }
 
 int main(int argc, char *argv[]) {
-    rw_mode mode = io;
-    io_width width = io_width_8;
-    daddr_t addr = 0;
+    rw_config *cfg = malloc(sizeof(rw_config));
     char *convert_checker = NULL;
     unsigned long val;
-    bool is_mode_setted = false;
+
+    cfg->address = 0x0;
+    cfg->data = 0x0;
+    cfg->mode = io;
+    cfg->width = io_width_8;
 
     debug_print(DEBUG_INFO, "Your have enter %d arguments\n", argc);
     for(size_t i = 0; i < argc; i++) {
@@ -85,15 +128,11 @@ int main(int argc, char *argv[]) {
     for(size_t i = 1; i < argc; i++) {
         val = strtoul(argv[i], &convert_checker, 0);
         if(*convert_checker == '\0') { /* argv[i] convert to digit success */
-            addr = val;
-            debug_print(DEBUG_INFO, "argv[%d] %-8s: 0x%x\n", i, "Address", addr);
+            if(!set_digit_args_to_config(cfg, val)) return 1;
             continue;
         }
 
-        if(!is_flag_supported(argv[i])) {
-            debug_print(DEBUG_ERROR, "argv[%d] flag: '%s' not support ", i, argv[i]);
-            return 1;
-        }
+        if(!is_flag_supported(argv[i])) return 1;
 
         if((strcasecmp("-h", argv[i]) == 0) || (strcasecmp("--help", argv[i]) == 0)) {
             show_help();
@@ -110,48 +149,13 @@ int main(int argc, char *argv[]) {
                 debug_print(DEBUG_ERROR, "Width can not empty\n");
                 return 1;
             }
-            width = strtoul(argv[i], &convert_checker, 0);
-            switch(width) {
-            case io_width_8:
-            case io_width_16:
-            case io_width_32:
-                debug_print(DEBUG_INFO, "argv[%d] %-8s: %d\n", i, "Width", width);
-                continue;
-            case io_width_64:
-            default:
-                debug_print(DEBUG_ERROR, "argv[%d] Width not support\n", i);
-                return 1;
-            }
+            if(!set_width_to_config(cfg, argv[i])) return 1;
         }
 
-        if(strcasecmp("-io", argv[i]) == 0) {
-            if(is_mode_setted) {
-                debug_print(DEBUG_ERROR, "argv[%d] Mode duplicated\n", i);
-                return 1;
-            }
-            mode = io;
-            is_mode_setted = true;
-            debug_print(DEBUG_INFO, "argv[%d] %-8s: %s\n", i, "Mode", "io");
-        }
-
-        if(strcasecmp("-mmio", argv[i]) == 0) {
-            if(is_mode_setted) {
-                debug_print(DEBUG_ERROR, "argv[%d] Mode duplicated\n", i);
-                return 1;
-            }
-            mode = mmio;
-            is_mode_setted = true;
-            debug_print(DEBUG_INFO, "argv[%d] %-8s: %s\n", i, "Mode", "mmio");
-        }
-
-        if(strcasecmp("-pci", argv[i]) == 0) {
-            if(is_mode_setted) {
-                debug_print(DEBUG_ERROR, "argv[%d] Mode duplicated\n", i);
-                return 1;
-            }
-            mode = pci;
-            is_mode_setted = true;
-            debug_print(DEBUG_INFO, "argv[%d] %-8s: %s\n", i, "Mode", "pci");
+        if((strcasecmp("-io", argv[i]) == 0) || (strcasecmp("-mmio", argv[i]) == 0)
+           || (strcasecmp("-pci", argv[i]) == 0) || (strcasecmp("-cc", argv[i]) == 0)
+           || (strcasecmp("-mc", argv[i]) == 0)) {
+            if(!set_mode_to_config(cfg, argv[i])) return 1;
         }
     }
     return 0;
