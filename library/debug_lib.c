8b9c7a98ae6739a788cb02b020d3c1e1f2744b12
Bao.Pang.ArchLinux <bao.pang@ufispace.com>
2022-12-15 09:32:53 +0800

    build(library): redirect warn, error, fatal message to stderr

--- library/debug_lib.c
+++ library/debug_lib.c
@@ -9,8 +9,13 @@ void debug_print(uint8_t debug_level, char *format, ...) {
     va_list marker;
 
     va_start(marker, format);
-    if(DEBUG_LEVEL >= debug_level) {
-        vprintf(format, marker);
+    if(DEBUG_LEVEL <= debug_level) {
+        return;
     }
+    if(debug_level <= DEBUG_WARN) { /*if error, output to stderr*/
+        vfprintf(stderr, format, marker);
+        return;
+    }
+    vprintf(format, marker);
     va_end(marker);
 }
